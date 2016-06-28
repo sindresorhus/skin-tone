ec6d0c88786960e3eca3d8e897547ad856261238
Marko Grešak <gresak.marko@gmail.com>
2016-06-29 01:28:38 +0200

    implement type handling if type is string, refactor and reuse previous code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 const emojiModifierBase = require('unicode-emoji-modifier-base');
+const snakeCase = require('lodash.snakecase');
 
 const skinTones = [
 	{
@@ -28,22 +29,50 @@ const skinTones = [
 	}
 ];
 
-module.exports = (emoji, type) => {
-	if (type > 5 || type < 0) {
-		throw new TypeError(`Expected \`type\` to be a number between 0 and 5, got ${type}`);
-	}
-
+function iterateSkinTones(emoji) {
 	// TODO: use this instead when targeting Node.js 6
 	// emoji = emoji.replace(/[\u{1f3fb}-\u{1f3ff}]/u, '');
 	skinTones.forEach(x => {
 		emoji = emoji.replace(x.color, '');
 	});
+	return emoji;
+}
 
-	if (emojiModifierBase.has(emoji.codePointAt(0)) && type !== 0) {
-		emoji += skinTones[type].color;
+function processEmoji(emoji, skinTone) {
+	if (emojiModifierBase.has(emoji.codePointAt(0)) && skinTones.indexOf(skinTone) > 0) {
+		emoji += skinTone.color;
 	}
 
 	return emoji;
+}
+
+function typeAsNumber(emoji, type) {
+	if (type > 5 || type < 0) {
+		throw new TypeError(`Expected \`type\` to be a number between 0 and 5, got ${type}`);
+	}
+
+	return skinTones[type];
+}
+
+function typeAsString(emoji, type) {
+	const typeAsName = snakeCase(type).toUpperCase();
+	const skinTone = skinTones.filter(x => x.name === typeAsName)[0];
+	if (!skinTone) {
+		throw new TypeError(`Expected \`type\` to be a string matching one of skin tone names, got ${type}`);
+	}
+
+	return skinTone;
+}
+
+module.exports = (emoji, type) => {
+	const fnByType = (() => {
+		switch (typeof type) {
+			case 'number': return typeAsNumber;
+			case 'string': return typeAsString;
+			default: return () => {};
+		}
+	})();
+	return processEmoji(iterateSkinTones(emoji), fnByType(emoji, type));
 };
 
 skinTones.forEach((x, i) => {
