6a875459945dc5c911cefc96b8cc084eb6734785
Marko Grešak <gresak.marko@gmail.com>
2016-06-28 22:06:41 +0200

    refactor to also export skin tone color names

--- test.js
+++ test.js
@@ -13,3 +13,12 @@ test(t => {
 	t.is(m('🐶', 5), '🐶');
 	t.is(m('👍🏿', 1), '👍🏻');
 });
+
+test('Color name constants', t => {
+	t.is(m.NONE, 0);
+	t.is(m.WHITE, 1);
+	t.is(m.CREAM_WHITE, 2);
+	t.is(m.LIGHT_BROWN, 3);
+	t.is(m.BROWN, 4);
+	t.is(m.DARK_BROWN, 5);
+});
