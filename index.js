4c5120c74eed2e18ddf0e1a0131fe7380e45ca42
Sindre Sorhus <sindresorhus@gmail.com>
2017-01-13 17:44:21 +0700

    Meta tweaks

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ module.exports = (emoji, type) => {
 		throw new TypeError(`Expected \`type\` to be a number between 0 and 5, got ${type}`);
 	}
 
-	// TODO: use this instead when targeting Node.js 6
+	// TODO: Use this instead when targeting Node.js 6
 	// emoji = emoji.replace(/[\u{1f3fb}-\u{1f3ff}]/u, '');
 	skinTones.forEach(x => {
 		emoji = emoji.replace(x.color, '');
