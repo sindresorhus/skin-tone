f1e9613d9e7885ff6527281b8fc021296c179f90
Amiran Ramishvili <amiran.ramishvili@gmail.com>
2024-01-03 12:53:05 +0400

    fix null match

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ export default function skinTone(emoji, tone) {
 
 	emoji = emoji.replaceAll(/[\u{1F3FB}-\u{1F3FF}]/ug, '');
 
-	if (tone !== 'none' && (emoji).match(emojiBaseModifierRegex).length < 3 && !twoFamilyEmojis.has(emoji)) {
+	if (tone !== 'none' && (emoji).match(emojiBaseModifierRegex)?.length < 3 && !twoFamilyEmojis.has(emoji)) {
 		const emojiArray = [...emoji];
 		emoji = '';
 
