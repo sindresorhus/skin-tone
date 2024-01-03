a28b60d4c4cd2f3e75298d8b54e1ebd325c0ba54
Amiran Ramishvili <amiran.ramishvili@gmail.com>
2024-01-03 12:33:04 +0400

    fix skin tone removal from two element emojis; cleanup

--- index.js
+++ index.js
@@ -7,29 +7,29 @@ const skinTones = new Map([
 	['darkBrown', '🏿']
 ]);
 
-const emojiBaseModifierRegex = /\p{Emoji_Modifier_Base}/ug
-const unicodeVariationSelector = '\uFE0F'
-const twoFamilyEmojis = ['👩‍👦', '👩‍👧', '👨‍👧', '👨‍👦']
+const emojiBaseModifierRegex = /\p{Emoji_Modifier_Base}/ug;
+const unicodeVariationSelector = '\uFE0F';
+const twoFamilyEmojis = ['👩‍👦', '👩‍👧', '👨‍👧', '👨‍👦'];
 
 export default function skinTone(emoji, tone) {
 	if (!skinTones.has(tone)) {
 		throw new TypeError(`Unexpected \`skinTone\` name: ${tone}`);
 	}
 
-	emoji = emoji.replace(/[\u{1F3FB}-\u{1F3FF}]/u, '');
+	emoji = emoji.replaceAll(/[\u{1F3FB}-\u{1F3FF}]/ug, '');
 
 	if (tone !== 'none' && (emoji).match(emojiBaseModifierRegex).length < 3 && !twoFamilyEmojis.includes(emoji)) {
 		emoji = [...emoji].reduce((res, char, idx, arr) => {
 			if (idx !== arr.length - 1 && char === unicodeVariationSelector) {
-				return res
+				return res;
 			}
-			res += char
+			res += char;
 			if (emojiBaseModifierRegex.test(char)) {
-				res += skinTones.get(tone)
+				res += skinTones.get(tone);
 			}
-			return res
-		}, '')
+			return res;
+		}, '');
 	}
 
-	return emoji
+	return emoji;
 }
