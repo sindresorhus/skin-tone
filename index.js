9523b6ac527adfcb1fc8956fafafc1e63c9dce35
Amiran Ramishvili <amiran.ramishvili@gmail.com>
2024-01-07 23:38:44 +0400

    add early return; update comments; move global regex to function level

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ const skinTones = new Map([
 	['darkBrown', '🏿'],
 ]);
 
-// This emoji modifier base \p{Emoji_Modifier_Base} is present in emojis that the skin tone can apply to
-const emojiBaseModifierRegex = /\p{Emoji_Modifier_Base}/ug;
 // Emoji presentation selector takes the same place as skin tone modifier https://unicode.org/reports/tr51/#composing_zwj_seq
-// So it should be removed if present, otherwise it causes issues like this '🕵🏼️‍♀️'
+// So it should be removed if present, otherwise it causes issues with emojis with several traits
+// For example "female-detective" turns into a detective with gender symbol next to it, instead of showing a female detective with set skin tone '🕵🏼️‍♀️'
 const emojiPresentationSelector = '\u{FE0F}';
 // Skin tones aren't supported for family emojis
 // Family emojis with 3+ person emojis this easily checked by the number of modifiable component emojis
@@ -25,27 +24,29 @@ export default function skinTone(emoji, tone) {
 
 	emoji = emoji.replaceAll(/[\u{1F3FB}-\u{1F3FF}]/ug, '');
 
-	// If tone is not 'none', emoji has less than 3 modifiable components (i.e. no families)
-	// And is not any of the two-person family emojis, then tone should be applied
-	if (tone !== 'none' && (emoji).match(emojiBaseModifierRegex)?.length < 3 && !twoFamilyEmojis.has(emoji)) {
-		let tonedEmoji = '';
+	// This emoji modifier base \p{Emoji_Modifier_Base} is present in emojis that the skin tone can apply to
+	const emojiBaseModifierRegex = /\p{Emoji_Modifier_Base}/ug;
 
-		for (const segment of emoji) {
-			// If this segment is emoji presentation selector it should not be added to toned emoji
-			if (segment === emojiPresentationSelector) {
-				continue;
-			}
-
-			tonedEmoji += segment;
-			// Tone should be applied to all modifiable components
-			// E.g. both persons in couple emojis, etc.
-			if (emojiBaseModifierRegex.test(segment)) {
-				tonedEmoji += skinTones.get(tone);
-			}
-		}
-
-		return tonedEmoji;
+	// If tone is 'none', emoji has > 2 modifiable componetns, or is a two-person family emoji skin tne should not be applied
+	if (tone === 'none' || (emoji).match(emojiBaseModifierRegex)?.length > 2 || twoFamilyEmojis.has(emoji)) {
+		return emoji;
 	}
 
-	return emoji;
+	let tonedEmoji = '';
+
+	for (const segment of emoji) {
+		// If this segment is emoji presentation selector it should not be added to toned emoji
+		if (segment === emojiPresentationSelector) {
+			continue;
+		}
+
+		tonedEmoji += segment;
+		// Tone should be applied to all modifiable components
+		// E.g. both persons in couple emojis, etc.
+		if (emojiBaseModifierRegex.test(segment)) {
+			tonedEmoji += skinTones.get(tone);
+		}
+	}
+
+	return tonedEmoji;
 }
